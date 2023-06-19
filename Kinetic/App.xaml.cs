045fcc5704f4d0e164c4c17a66861cdbca3f79c6
aliesmaelfard <aliesmaelfard@gmail.com>
2023-06-19 21:48:14 +0330

    Add project files.

---
+++ Kinetic/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Kinetic.Presentation.Custom_Controler;
+
+namespace Kinetic;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+
+		Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(CustomEntry),
+			(handler, view) =>
+			{
+#if __ANDROID__
+			handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+#elif __IOS__
+				handler.PlatformView.BackgroundColor = UIKit.UIColor.Clear;
+				handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#endif
+			} );
+	}
+}
